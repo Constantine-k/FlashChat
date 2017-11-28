14edaf69ff0bcb235f25b4f9a18b7f871e89b836
Konstantin Konstantinov <konstantin.k.bm@gmail.com>
2017-11-28 02:10:09 +0200

    Implement fetching messages from Firebase

--- Flash Chat/ChatViewController.swift
+++ Flash Chat/ChatViewController.swift
@@ -11,6 +11,8 @@ import Firebase
 
 class ChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
     
+    var messageArray = [Message]()
+    
     @IBOutlet var heightConstraint: NSLayoutConstraint!
     @IBOutlet var sendButton: UIButton!
     @IBOutlet var messageTextfield: UITextField!
@@ -32,6 +34,7 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
         messageTableView.register(UINib(nibName: "MessageCell", bundle: nil), forCellReuseIdentifier: "customMessageCell")
         
         configureTableView()
+        retreiveMessages()
     }
 
     ///////////////////////////////////////////
@@ -39,15 +42,15 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     //MARK: - TableView DataSource Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return messageArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customMessageCell", for: indexPath) as! CustomMessageCell
         
-        let messageArray = ["Message 1", "Message 2", "Message 3"]
-        
-        cell.messageBody.text = messageArray[indexPath.row]
+        cell.messageBody.text = messageArray[indexPath.row].messageBody
+        cell.senderUsername.text = messageArray[indexPath.row].sender
+        cell.avatarImageView.image = UIImage(named: "egg")
         
         return cell
     }
@@ -90,15 +93,43 @@ class ChatViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     
     @IBAction func sendPressed(_ sender: AnyObject) {
+        sendButton.isEnabled = false
+        messageTextfield.isEnabled = false
         
+        guard let sender = Auth.auth().currentUser?.email,
+            let messageBody = messageTextfield.text else { return }
         
-        //TODO: Send the message to Firebase and save it in our database
+        let messagesDB = Database.database().reference().child("Message")
+        let messageDictionary = ["Sender": sender, "MessageBody": messageBody]
         
-        
+        messagesDB.childByAutoId().setValue(messageDictionary) { [weak self] (error, reference) in
+            if error != nil {
+                print("Error: \(error!)")
+            } else {
+                print("Message sent successfully!")
+                
+                self?.sendButton.isEnabled = true
+                self?.messageTextfield.isEnabled = true
+                self?.messageTextfield.text = ""
+            }
+        }
     }
     
-    //TODO: Create the retrieveMessages method here:
-    
+    /// Fetches messages from Firebase database
+    func retreiveMessages() {
+        let messagesDB = Database.database().reference().child("Message")
+        
+        messagesDB.observe(.childAdded) { (snapshot) in
+            guard let snapshotValue = snapshot.value as? Dictionary<String, String>,
+                let text = snapshotValue["MessageBody"],
+                let sender = snapshotValue["Sender"] else { return }
+            
+            self.messageArray.append(Message(sender: sender, messageBody: text))
+            
+            self.configureTableView()
+            self.messageTableView.reloadData()
+        }
+    }
     
 
     
--- Flash Chat/Message.swift
+++ Flash Chat/Message.swift
@@ -4,7 +4,7 @@
 //
 //  This is the model class that represents the blueprint for a message
 
-class Message {
-    var sender = ""
-    var messageBody = ""
+struct Message {
+    var sender: String
+    var messageBody: String
 }
